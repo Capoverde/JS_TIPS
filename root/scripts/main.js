132c2eb799ac59c8ecb061113eb7929d8d23ab60
Piotr <piotr_as@yahoo.com>
2018-07-20 15:54:59 +0200

    root folder added

---
+++ root/scripts/main.js
@@ -0,0 +1,41 @@
+// 20 String methods //
+
+var stringOne = "freeCodeCamp is the best place to learn";
+var stringTwo = " forntend and backend development";
+
+// #1 charAt()
+console.log(stringOne.charAt(0));
+
+// #2 charCodeAt()
+console.log(stringOne.charCodeAt(1));
+
+// #3 concat()
+console.log(stringOne.concat(stringTwo));
+
+// #4 endsWith()
+console.log(stringOne.endsWith('learn'));
+
+// #5 fromCharCode()
+console.log(String.fromCharCode(114));
+
+// #6 includes()
+
+
+
+// #7 indexOf()
+
+
+
+//  #8 lastIndexOf()
+
+
+//  #9 match()
+
+
+//  #10 repeat()
+
+
+//  #11 replace()
+
+
+
