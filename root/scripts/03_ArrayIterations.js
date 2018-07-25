9951d6e38b8e5cf3bc45992ac5a37bf0746792ed
Piotr <piotr_as@yahoo.com>
2018-07-25 12:29:03 +0200

    finsh it

---
+++ root/scripts/03_ArrayIterations.js
@@ -0,0 +1,38 @@
+// Array iteration: 8 methods
+
+//-------- #1 forEach --------- //
+
+// [1,2,3].forEach(function(item, index){
+//     console.log(item, index); 
+// });
+// >>
+// 1 0
+// 2 1
+// 3 2
+
+//-------- #2 map ----------- //
+
+const three = [1,2,3];
+const doubled = three.map(function(item){
+    return item * 2;
+});
+console.log(doubled);
+
+
+
+// #3 filter
+
+
+// #4 reduce
+
+
+// #5 some
+
+
+// 6 every
+
+
+// #7 find
+
+
+// #8
